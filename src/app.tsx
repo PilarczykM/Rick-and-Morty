cc1a555dc738ca17a1b2b5738e2aa6c84f128459
Lurppisek <marcinpilarczyk@me.com>
2022-12-05 21:13:20 +0100

    Add react-query
    Use react query in application

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,17 @@
 import './app.css'
+import { QueryClientProvider, QueryClient} from 'react-query'
 import { Characters } from './components/Characters'
+
+const queryClient = new QueryClient()
 
 export function App() {
   return (
-    <div className='App'>
-      <div className='container'>
+    <div className="App">
+      <div className="container">
         <h1>Rick and Morty</h1>
-        <Characters />
+        <QueryClientProvider client={queryClient}>
+          <Characters />
+        </QueryClientProvider>
       </div>
     </div>
   )
--- src/components/Characters.tsx
+++ src/components/Characters.tsx
@@ -1,29 +1,26 @@
-import { useEffect, useState } from 'preact/hooks'
-import { Character, CharacterResponse, Info } from './types'
+import { useQuery } from 'react-query'
+import { CharacterResponse } from './types'
 
 export const Characters = () => {
-  const [characters, setCharacters] = useState<Character[]>([])
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [info, setInfo] = useState<Info>()
-
-  useEffect(() => {
-    const controller = new AbortController()
-    const signal = controller.signal
-    fetchCharacters(signal)
-
-    return () => {
-      controller.abort()
-    }
-  }, [])
-
-  const fetchCharacters = async (signal: AbortSignal) => {
-    const response = await fetch('https://rickandmortyapi.com/api/character', { signal })
-    if (response.ok) {
-      const characterResponse: CharacterResponse = await response.json()
-      setCharacters(characterResponse.results)
-      setInfo(characterResponse.info)
-    }
+  const fetchCharacters = async () => {
+    const response = await fetch('https://rickandmortyapi.com/api/character')
+    return response.json()
   }
 
-  return <div>{characters && characters.map((character, index) => <li key={index}>{character.name}</li>)}</div>
+  const { data, status } = useQuery<CharacterResponse, Error>('characters', fetchCharacters)
+  if (status === 'loading') {
+    return <div>Loading...</div>
+  }
+
+  if (status === 'error') {
+    return <div>Error...</div>
+  }
+
+  return (
+    <div>
+      {status === 'success' && data?.results.map((character, index) => (
+        <p key={index}>{character.name}</p>
+      ))}
+    </div>
+  )
 }
