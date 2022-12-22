fd163f8f0999752965c90b8e82e002ab7189248d
Lurppisek <marcinpilarczyk@me.com>
2022-12-22 20:12:21 +0100

    Create Static nextapp

---
+++ nextjs-static/next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'rickandmortyapi.com',
+        pathname: '/api/character/avatar/**',
+      },
+    ],
+  },
+}
+
+module.exports = nextConfig
